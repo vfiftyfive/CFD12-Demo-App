69ef0fc6a7674e98f8c0411824e96b52dda8c9d9
vfiftyfive <viftyfive@gmail.com>
2021-11-02 14:57:10 +0000

    remove test

--- delete-all.sh
+++ delete-all.sh
@@ -7,5 +7,8 @@ echo -e "Deleting PVC..."
 kubectl delete pvc --all 2> /dev/null
 echo -e "Deleting Jobs..."
 kubectl delete job --all 2> /dev/null
-echo -e "Deleting Service..."
+echo -e "Deleting Services..."
 kubectl delete svc mongodb 2> /dev/null
+kubectl delete svc marvel-frontend 2> /dev/null
+echo -e  "Deleting Deployment..."
+kubectl delete deploy marvel-frontend 2> /dev/null
